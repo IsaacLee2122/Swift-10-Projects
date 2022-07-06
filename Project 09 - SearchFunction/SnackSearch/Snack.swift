f100505e21c2825b90dd08c170146a158a933273
Isaac Lee <99067082+IsaacLee2122@users.noreply.github.com>
2022-07-06 12:06:05 -0500

    Add files via upload

---
+++ Project 09 - SearchFunction/SnackSearch/Snack.swift
@@ -0,0 +1,13 @@
+
+
+import Foundation
+
+class Snack {
+    var category: String
+    var name: String
+    
+    init(category: String, name: String) {
+        self.category = category
+        self.name = name
+    }
+}
